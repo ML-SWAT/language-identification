bd5e2ff79a9cd926265b9c1e8ec7c90cfa90a8d0
lonelyjohner <lonelyjohner@163.com>
2015-04-13 20:53:34 -0400

    Revert "test on 100 chinese cases and get 100% correct rate"
    This reverts commit 445c73e2d99a8277bc4ac739481a31f2ada8b2f8.

--- MFCC/listFileM4a.m
+++
@@ -1,9 +0,0 @@
-function res=listFileM4a(path)
-fileList = dir(path);
-res=[];
-for i =1:length(fileList)
-    startIndex = regexp(fileList(i).name,'.m4a', 'once');
-    if ~isempty(startIndex)
-        res=[res,fileList(i)];
-    end
-end
